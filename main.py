e2bf7958196795cd85cbc0160a82d568b8752932
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2014-12-30 16:40:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import bottle
+import psycopg2
 from bottle import request
 
 app = bottle.Bottle()
@@ -7,7 +8,7 @@ app = bottle.Bottle()
 
 @app.hook('before_request')
 def auth_hook():
-    if request.auth == ('gimmi', 'secret'):
+    if request.auth and auth_user(*request.auth):
         request['app.user'] = request.auth[0]
     else:
         raise bottle.HTTPResponse(status=401, headers={'WWW-Authenticate': 'Basic realm="Please Login"'})
@@ -26,6 +27,14 @@ def static(filepath):
     response.set_header('Pragma', 'no-cache')
     response.set_header('Expires', '0')
     return response
+
+
+def auth_user(name, password):
+    conn = psycopg2.connect('host=localhost port=5432 dbname=prova user=postgres password=postgres')
+    cur = conn.cursor()
+    cur.execute('select id, name, password from users where name = %s and password = %s', (name, password))
+    row = cur.fetchone()
+    return bool(row)
 
 
 if __name__ == '__main__':
