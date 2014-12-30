7508a0d0a1ad5f2ec30144a5eece6136df4fdf58
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2014-12-30 13:18:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
-from bottle import Bottle, template
+import os
+import bottle
+from bottle import request
 
-app = Bottle()
+app = bottle.Bottle()
 
-@app.route('/hello/<name>')
-def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+
+@app.hook('before_request')
+def auth_hook():
+    if request.auth == ('gimmi', 'secret'):
+        request['app.user'] = request.auth[0]
+    else:
+        raise bottle.HTTPResponse(status=401, headers={'WWW-Authenticate': 'Basic realm="Please Login"'})
+
+
+@app.get('/')
+def index():
+    return static('index.html')
+
+
+@app.get('/<filepath:path>')
+def static(filepath):
+    root = os.path.join(os.path.dirname(os.path.realpath(__file__)))
+    response = bottle.static_file(filepath, root=root)
+    response.set_header('Cache-Control', 'no-cache, no-store, must-revalidate')
+    response.set_header('Pragma', 'no-cache')
+    response.set_header('Expires', '0')
+    return response
+
 
 if __name__ == '__main__':
     app.run()
