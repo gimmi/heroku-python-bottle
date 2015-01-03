3df311f8bcbb4bde1e3d7a3363ed1a12b41e79f1
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2015-01-03 16:23:00 +0100

    Implemented DB connection pool and auth trough plugins

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 import os
 import bottle
-from urllib.parse import urlparse
-import psycopg2
-from bottle import request
+import support
 
+
+conn_pool = support.create_conn_pool()
 app = bottle.Bottle()
-
-
-@app.hook('before_request')
-def auth_hook():
-    if request.auth and auth_user(*request.auth):
-        request['app.user'] = request.auth[0]
-    else:
-        raise bottle.HTTPResponse(status=401, headers={'WWW-Authenticate': 'Basic realm="Please Login"'})
+app.install(support.DbPlugin(conn_pool))
+app.install(support.AuthPlugin(conn_pool))
 
 
 @app.get('/')
@@ -28,22 +22,6 @@ def static(filepath):
     response.set_header('Pragma', 'no-cache')
     response.set_header('Expires', '0')
     return response
-
-
-def auth_user(name, password):
-    url = os.environ.get('DATABASE_URL', 'postgresql://postgres:postgres@localhost/hrp')
-    url = urlparse(url)
-    conn = psycopg2.connect(
-        host=url.hostname,
-        port=url.port,
-        database=url.path[1:],
-        user=url.username,
-        password=url.password
-    )
-    cur = conn.cursor()
-    cur.execute('select id, name, password from users where name = %s and password = %s', (name, password))
-    row = cur.fetchone()
-    return bool(row)
 
 
 if __name__ == '__main__':
---
+++ support.py
@@ -0,0 +1,69 @@
+import psycopg2.pool
+import os
+from urllib.parse import urlparse
+import bottle
+
+
+def create_conn_pool():
+    url = os.environ.get('DATABASE_URL', 'postgresql://postgres:postgres@localhost/hrp')
+    url = urlparse(url)
+    return psycopg2.pool.ThreadedConnectionPool(1, 20, **dict(
+        host=url.hostname,
+        port=url.port,
+        database=url.path[1:],
+        user=url.username,
+        password=url.password
+    ))
+
+
+class DbPlugin(object):
+    name = 'db'
+    api = 2
+
+    def __init__(self, conn_pool):
+        self._conn_pool = conn_pool
+
+    def apply(self, callback, route):
+        if 'db' not in route.get_callback_args():
+            return callback
+
+        def wrapper(*args, **kwargs):
+            kwargs['db'] = self._conn_pool.getconn()
+            try:
+                return callback(*args, **kwargs)
+            finally:
+                self._conn_pool.putconn(kwargs['db'])
+
+        return wrapper
+
+
+class AuthPlugin(object):
+    name = 'auth'
+    api = 2
+
+    def __init__(self, conn_pool):
+        self._conn_pool = conn_pool
+
+    def apply(self, callback, route):
+        if 'user' not in route.get_callback_args():
+            return callback
+
+        def wrapper(*args, **kwargs):
+            auth = bottle.request.auth
+            if auth and self._auth_user(*auth):
+                kwargs['user'] = auth[0]
+                return callback(*args, **kwargs)
+            else:
+                return bottle.Response(status=401, headers={'WWW-Authenticate': 'Basic realm="Please Login"'})
+
+        return wrapper
+
+    def _auth_user(self, name, password):
+        conn = self._conn_pool.getconn()
+        try:
+            with conn.cursor() as cur:
+                cur.execute('select id, name, password from users where name = %s and password = %s', (name, password))
+                row = cur.fetchone()
+                return bool(row)
+        finally:
+            self._conn_pool.putconn(conn)
